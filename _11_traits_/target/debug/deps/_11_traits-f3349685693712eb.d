8afa7b613febe35718db68bfd031f4a71811c88d
maxfer1221 <maxfer1221@gmail.com>
2021-10-31 02:26:15 -0400

    rename

---
+++ _11_traits_/target/debug/deps/_11_traits-f3349685693712eb.d
@@ -0,0 +1,5 @@
+/home/maximo/Documents/rust tut/_11_traits/target/debug/deps/_11_traits-f3349685693712eb: src/main.rs
+
+/home/maximo/Documents/rust tut/_11_traits/target/debug/deps/_11_traits-f3349685693712eb.d: src/main.rs
+
+src/main.rs:
